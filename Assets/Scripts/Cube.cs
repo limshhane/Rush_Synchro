54dbb3aafc7cbc8449322a457f83cbec99286f85
HH <49613066+limshhane@users.noreply.github.com>
2021-05-26 21:51:03 +0200

    First

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    static public List<Cube> list { get; private set; } = new List<Cube>();
+
+    [SerializeField]public float tumblingDuration = 0.2f;
+    bool isTumbling = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        list.Add(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTumbling)
+        {
+            StartCoroutine(Tumble(Vector3.right));
+        }
+    }
+
+    IEnumerator Tumble(Vector3 direction)
+    {
+        isTumbling = true;
+
+        var rotAxis = Vector3.Cross(Vector3.up, direction);
+        var pivot = (transform.position + Vector3.down * 0.5f) + direction * 0.5f;
+
+        var startRotation = transform.rotation;
+        var endRotation = Quaternion.AngleAxis(90.0f, rotAxis) * startRotation;
+
+        var startPosition = transform.position;
+        var endPosition = transform.position + direction;
+
+        var rotSpeed = 90.0f / tumblingDuration;
+        var t = 0.0f;
+
+        while (t < tumblingDuration)
+        {
+            t += Time.deltaTime;
+            if (t < tumblingDuration)
+            {
+                transform.RotateAround(pivot, rotAxis, rotSpeed * Time.deltaTime);
+                yield return null;
+            }
+            else
+            {
+                transform.rotation = endRotation;
+                transform.position = endPosition;
+            }
+        }
+
+        isTumbling = false;
+    }
+
+
+}
